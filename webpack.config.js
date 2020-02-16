df6a954fe2c92dc06ccc96b2c83bc9b110908788
yongming <zhuangyongming1900@gmail.com>
2020-02-16 19:07:43 +0800

    Add css loaders to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
 
 module.exports = {
   entry: {
-     polyfills: './src/polyfills.js',
-     index: './src/index.js',
-   },
-   output: {
+    polyfills: "./src/polyfills.js",
+    index: "./src/index.js"
+  },
+  output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
   module: {
-    rules: [{ test: /\.txt$/, use: "raw-loader" }]
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
   plugins: [new HtmlWebpackPlugin({ template: "index.html" })]
 };
