5841ff7efd9936db100bcae729cf2c650f7c063f
yongming <zhuangyongming1900@gmail.com>
2020-02-16 15:42:00 +0800

    Create index html and index js files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = "hello world";
 
   return element;
 }
 
 document.body.appendChild(component());
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(json => {
+    console.log(
+      "We retrieved some data! AND we're confident it will work on a variety of browser distributions."
+    );
+    console.log(json);
+  })
+  .catch(error =>
+    console.error("Something went wrong when fetching this data: ", error)
+  );
