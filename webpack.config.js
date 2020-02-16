4bf74b527baa19f84c1403ee943de18f2817a06b
yongming <zhuangyongming1900@gmail.com>
2020-02-16 15:41:03 +0800

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+
+module.exports = {
+  entry: {
+     polyfills: './src/polyfills.js',
+     index: './src/index.js',
+   },
+   output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [{ test: /\.txt$/, use: "raw-loader" }]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "index.html" })]
+};
