a597e33b0412496edc48bc3f41eaf6b77c055922
yongming <zhuangyongming1900@gmail.com>
2020-02-16 19:08:13 +0800

    Add styles to index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import "./style.css";
+
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = "hello world";
+  element.classList.add("hello");
 
   return element;
 }
