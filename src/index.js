2d3e75edd14c83246033ee9d6667b972ff3859e2
yongming <zhuangyongming1900@gmail.com>
2020-02-16 19:28:13 +0800

    Add file loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import Icon from "./icon.jpg";
 
 function component() {
   const element = document.createElement("div");
@@ -6,6 +7,10 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = "hello world";
   element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
